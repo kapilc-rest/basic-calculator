ca56c92c25bdb6d2b5b736d51d1d36602ba60ed9
kapilc-rest <311136214+kapilc-rest@users.noreply.github.com>
2026-09-03 06:40:24 +0000

    Fix operate function

--- script.js
+++ script.js
@@ -19,12 +19,16 @@ let operand1, operation, operand2;
 function operate(num1, operator, num2) {
     switch (operator) {
         case '+' :
-            add(num1, num2);
+            return add(num1, num2);
+            break;
         case '-' :
-            subtract(num1, num2);
+            return subtract(num1, num2);
+            break;
         case '*' :
-            multiply(num1, num2);
+            return multiply(num1, num2);
+            break;
         case '/' : 
-            divide(num1, num2);
+            return divide(num1, num2);
+            break;
     }
 }
