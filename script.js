e106f653426a8eb3b720c069d947a2ce6dd1b96d
kapilc-rest <311136214+kapilc-rest@users.noreply.github.com>
2026-09-03 16:10:37 +0000

    Make minor changes

--- script.js
+++ script.js
@@ -17,7 +17,6 @@ function divide(a,b) {
 let operand1="", operation="", operand2="", result = "";
 
 function operate(num1, operator, num2) {
-    show.innerHTML = " ";
     switch (operator) {
         case '+' :
             return add(num1, num2);
@@ -29,66 +28,6 @@ function operate(num1, operator, num2) {
             return divide(num1, num2);
     }
 }
-
-// const show = document.querySelector("#show");
-
-// let objNum = {}, index = 0;
-
-// const numbers = document.querySelectorAll("button.number");
-// numbers.forEach( number => { 
-//     number.addEventListener("click",() => {
-//         enable(operators);
-//         if(!(index in obj)){
-//             obj[index] = "";
-//             show.textContent = "";
-//         }
-//         show.textContent += number.textContent;
-//         obj[index] += number.textContent;
-//     })
-// });
-
-// const listOperator = [ '+', '-', '*', '/'];
-
-// function disable(list) {
-//     list.forEach(item => { item.disabled = true});
-// }
-
-// function enable(list) {
-//     list.forEach(item => item.disabled = false);
-// }
-
-// const operators = document.querySelectorAll("button.operator");
-// operators.forEach( operator => {
-//     operator.addEventListener("click", () => {
-//         // index++;
-//         // show.textContent = `${operator.textContent}`
-//         // obj[index] = operator.textContent;
-//         // disable(operators);
-//         // enable(numbers);
-//         // index++;
-//     })
-// })
-
-// const equal = document.querySelector("#equal");
-// equal.addEventListener("click", () => {
-//     console.log(obj);
-//     let result = operate(+obj[0], obj[1], +obj[2]);
-//     obj[0] = result;
-//     show.textContent = result;
-//     obj[1] = "";
-//     obj[2] = "";
-//     index = 0;
-// });
-
-// const clear = document.querySelector("#clear");
-// clear.addEventListener("click", () => {
-//     show.innerHTML = "";
-//     enable(operators);
-//     disable(numbers);
-//     obj={};
-//     index = 0;
-//     console.log(obj);
-// })
 
 const numbers = document.querySelectorAll("button.number");
 const operators = document.querySelectorAll("button.operator");
