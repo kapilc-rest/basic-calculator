08e40782b46dbcf047e503989cba9ed8e7a8f39c
kapilc-rest <311136214+kapilc-rest@users.noreply.github.com>
2026-09-03 06:25:03 +0000

    Add operate function

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ function multiply(a,b) {
 function divide(a,b) {
     return b === 0 ? "nope" : (a/b);
 }
+
+let operand1, operation, operand2;
+
+function operate(num1, operator, num2) {
+    switch (operator) {
+        case '+' :
+            add(num1, num2);
+        case '-' :
+            subtract(num1, num2);
+        case '*' :
+            multiply(num1, num2);
+        case '/' : 
+            divide(num1, num2);
+    }
+}
