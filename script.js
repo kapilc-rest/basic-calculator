365aa507552e9c25764218ced5900b9263d2064d
kapilc-rest <311136214+kapilc-rest@users.noreply.github.com>
2026-09-03 13:15:33 +0000

    Add nodes and event listeners

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function divide(a,b) {
     return b === 0 ? "nope" : (a/b);
 }
 
-let operand, operation, operand2;
+let operand1="", operation="", operand2="";
 
 function operate(num1, operator, num2) {
     show.innerHTML = " ";
@@ -30,55 +30,82 @@ function operate(num1, operator, num2) {
     }
 }
 
-const show = document.querySelector("#show");
+// const show = document.querySelector("#show");
 
-let obj = {}, index = 0;
+// let objNum = {}, index = 0;
+
+// const numbers = document.querySelectorAll("button.number");
+// numbers.forEach( number => { 
+//     number.addEventListener("click",() => {
+//         enable(operators);
+//         if(!(index in obj)){
+//             obj[index] = "";
+//             show.textContent = "";
+//         }
+//         show.textContent += number.textContent;
+//         obj[index] += number.textContent;
+//     })
+// });
+
+// const listOperator = [ '+', '-', '*', '/'];
+
+// function disable(list) {
+//     list.forEach(item => { item.disabled = true});
+// }
+
+// function enable(list) {
+//     list.forEach(item => item.disabled = false);
+// }
+
+// const operators = document.querySelectorAll("button.operator");
+// operators.forEach( operator => {
+//     operator.addEventListener("click", () => {
+//         // index++;
+//         // show.textContent = `${operator.textContent}`
+//         // obj[index] = operator.textContent;
+//         // disable(operators);
+//         // enable(numbers);
+//         // index++;
+//     })
+// })
+
+// const equal = document.querySelector("#equal");
+// equal.addEventListener("click", () => {
+//     console.log(obj);
+//     let result = operate(+obj[0], obj[1], +obj[2]);
+//     obj[0] = result;
+//     show.textContent = result;
+//     obj[1] = "";
+//     obj[2] = "";
+//     index = 0;
+// });
+
+// const clear = document.querySelector("#clear");
+// clear.addEventListener("click", () => {
+//     show.innerHTML = "";
+//     enable(operators);
+//     disable(numbers);
+//     obj={};
+//     index = 0;
+//     console.log(obj);
+// })
 
 const numbers = document.querySelectorAll("button.number");
-numbers.forEach( number => { 
-    number.addEventListener("click",() => {
-        enable(operators);
-        if(!(index in obj)){
-            obj[index] = "";
-            show.textContent = "";
-        }
-        show.textContent += number.textContent;
-        obj[index] += number.textContent;
-    })
-});
-
-const listOperator = [ '+', '-', '*', '/'];
-
-function disable(list) {
-    list.forEach(item => { item.disabled = true});
-}
-
-function enable(list) {
-    list.forEach(item => item.disabled = false);
-}
-
 const operators = document.querySelectorAll("button.operator");
-operators.forEach( operator => {
-    operator.addEventListener("click", () => {
-        index++;
-        show.textContent = `${operator.textContent}`
-        obj[index] = operator.textContent;
-        disable(operators);
-        index++;
-    })
-})
+const equal = document.querySelector("button.equal");
+const clear = document.querySelector("button.clear");
+const show = document.querySelector("#show");
 
-const equal = document.querySelector("#equal");
-equal.addEventListener("click", () => {
-    console.log(obj);
-    show.textContent = operate(+obj[0], obj[1], +obj[2]);
+numbers.forEach(number => {
+    number.addEventListener("click", () => {
+        operand1 === "" ? operand1 = number : operand2 = number;
+        show.textContent += number.textContent;
+    })
 });
 
-const clear = document.querySelector("#clear");
-clear.addEventListener("click", () => {
-    show.innerHTML = "";
-    enable(operators);
-    obj={};
-    index = 0;
-    console.log(obj);
-})
+operators.forEach( operator => {
+    operator.addEventListener("click", () =>{
+        operand1 === "" ? operation = "" : operation = operator;
+        operation !== "" ? show.textContent = operate(operand1, operation,operand2) : 0;
+    })
+});
